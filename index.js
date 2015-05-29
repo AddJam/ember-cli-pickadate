80d032fcb4549dbd9335dbb6bcfeb5146d1b0107
Chris Sloey <chris@addjam.com>
2015-05-29 15:41:35 +0100

    Fix bower import

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    app.import('bower_components/pickadate/lib/picker.js');
-    app.import('bower_components/pickadate/lib/picker.date.js');
-    app.import('bower_components/pickadate/lib/picker.time.js');
-    app.import('bower_components/pickadate/lib/themes/default.css');
-    app.import('bower_components/pickadate/lib/themes/default.date.css');
-    app.import('bower_components/pickadate/lib/themes/default.time.css');
+    app.import(app.bowerDirectory + '/pickadate/lib/picker.js');
+    app.import(app.bowerDirectory + '/pickadate/lib/picker.date.js');
+    app.import(app.bowerDirectory + '/pickadate/lib/picker.time.js');
+    app.import(app.bowerDirectory + '/pickadate/lib/themes/default.css');
+    app.import(app.bowerDirectory + '/pickadate/lib/themes/default.date.css');
+    app.import(app.bowerDirectory + '/pickadate/lib/themes/default.time.css');
   }
 };
