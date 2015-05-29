f063213a37f69b095abc05aba6fa889a31f40ee9
Chris Sloey <chris@addjam.com>
2015-05-29 15:26:51 +0100

    Import required files

--- index.js
+++ index.js
@@ -2,5 +2,16 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-pickadate'
+  name: 'ember-cli-pickadate',
+  included: function(app) {
+    this._super.included(app);
+
+    app.import('bower_components/pickadate/lib/picker.js');
+    app.import('bower_components/pickadate/lib/picker.date.js');
+    app.import('bower_components/pickadate/lib/picker.time.js');
+    app.import('bower_components/pickadate/lib/themes/default.css');
+    app.import('bower_components/pickadate/lib/themes/default.date.css');
+    app.import('bower_components/pickadate/lib/themes/default.time.css');
+  }
+};
 };
